cf1411ec7b1f030700ff09033159b891cf371d46
Manilka Navod <manilka.projects@gmail.com>
2023-10-24 07:33:05 +0530

    basic setup

--- OnlineHelpDesk/src/com/dbUtil/DatabaseManager.java
+++ OnlineHelpDesk/src/com/dbUtil/DatabaseManager.java
@@ -1,5 +1,33 @@
 package com.dbUtil;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseManager {
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/HelpDeskDB";
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "password";
+	
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+		} catch(ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Database Failed to load.");
+		}
+	}
+	
+	public static void closeConnection(Connection connection) {
+		if (connection != null) {
+			try {
+				connection.close();
+			}
+			catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
--- OnlineHelpDesk/src/com/model/RefImage.java
+++ OnlineHelpDesk/src/com/model/RefImage.java
@@ -1,5 +1,37 @@
 package com.model;
 
 public class RefImage {
+	private int imageId;
+	private int ticketId;
+	private String path;
+	
+	public RefImage(int ticketId, String path) {
+		this.ticketId = ticketId;
+		this.path = path;
+	}
 
+	public int getImageId() {
+		return imageId;
+	}
+
+	public void setImageId(int imageId) {
+		this.imageId = imageId;
+	}
+
+	public int getTicketId() {
+		return ticketId;
+	}
+
+	public void setTicketId(int ticketId) {
+		this.ticketId = ticketId;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
 }
